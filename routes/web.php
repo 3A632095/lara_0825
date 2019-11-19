f1e56fa9638b882013fad1a2572f17c65d32d861
3A632095 <s3a632095@student.ncut.edu.tw>
2019-11-19 21:21:10 +0800

    練習 5：刪除多筆資料

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,49 @@
 */
 
 Route::get('/', function () {
+
+//    \App\Post::create([
+//        'title'=>'test title',
+//        'content'=>'test content',
+//    ]);
+
+//    $post=new\App\Post();
+//    $post->title='testtitle';
+//    $post->content='testcontent';
+//    $post->save();
+
+//   查詢資料
+
+//    $posts=\App\Post::all();
+//    dd($posts);
+
+//    $post = \App\Post::find(1);
+//    dd($post);
+
+//    $posts = \App\Post::where('id','<',10)->orderBy('id','DESC')->get();
+//    dd($posts);
+
+//   更新資料
+
+//      $post = \App\Post::find(1);
+//      $post->update([
+//          'title' =>'update title',
+//          'content'=>'update content',
+//      ]);
+
+//        $post =\App\Post::find(1);
+//        $post->title='saved title';
+//        $post->content='save content';
+//        $post->save();
+
+//     刪除資料
+
+//    $post =\App\Post::find(1);
+//    $post->delete();
+
+//    \App\Post::destroy(2);
+    \App\Post::destroy(3,5,7);
+
     return view('welcome');
 });
 
